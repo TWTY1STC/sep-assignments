1077e64add437b1c2aa18b791e40ec5d3085c930
twty1stc <liza.njuguna@gmail.com>
2017-01-27 23:40:51 +0000

    conceptualize linked lists and nodes

--- 03-linked-lists/node.rb
+++ 03-linked-lists/node.rb
@@ -1,7 +1,14 @@
 class Node
+=begin
+Elements in a Linked List are not stored next to one another in memory. 
+A series of nodes compose every Linked List and each node contains a piece of data 
+and the address (reference) of the next node in the list.
+=end
   attr_accessor :next
   attr_accessor :data
 
   def initialize(data)
+    @data = data
+    self.next = null
   end
 end
