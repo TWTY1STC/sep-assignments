03213f13bfc2ad12fffd429c9cb0d03ca5393135
twty1stc <liza.njuguna@gmail.com>
2017-02-18 00:56:52 +0000

    correct inbound return

--- 01-introduction-to-data-structures/screen/screen.rb
+++ 01-introduction-to-data-structures/screen/screen.rb
@@ -9,45 +9,38 @@ class Screen
   def initialize(width, height)
     @width = width
     @height = height
-    self.matrix #= Array.new(@height, Array.new(@width))
+    self.matrix = Array.new(height, Array.new(width))
     #at init matrix holds one container Array that will house other Arrays with pixels
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
-    puts pixel
     if inbounds(x, y)
-      @width = x
-      @height = y
-      return self.matrix = Array.new(@height, Array.new(@width, pixel))
+      #@width = x
+      #@height = y
+      matrix[x][y] = pixel
+      return matrix
+      #return matrix #= Array.new(@height, Pixel.new(@width))
       #pixel = Array.new(@width)
       #return self.matrix = Array.new(@height, pixel)
       #self.matrix << pixel[x][y]
     end
-    
-   
   end
-=begin
-[[ , ,]
- [ . .]
- [ , ,]]
-=end
+
   def at(x, y)
-    #if self.matrix
-      return pixel[x][y]
-    #end
-    #return pixel[x][y]
-    
+    if !matrix.empty?
+      return matrix[x][y]
+    end
   end
 
   private
 
   def inbounds(x, y)
     #return nil when at(x,y) are negative or...
-    if x < 0 || y < 0
+    if x < 0 || y < 0 || x > @width || y > @height
       return nil
     else
-      return
+      return true
     end
   end
 
