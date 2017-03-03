06f3926c7bf4af5b1f2a57818710bb602c6d90e7
twty1stc <liza.njuguna@gmail.com>
2017-03-03 23:27:52 +0000

    work on delete, print etc

--- 03-linked-lists/linked_list.rb
+++ 03-linked-lists/linked_list.rb
@@ -21,72 +21,33 @@ class LinkedList
       temp.next = node
       @tail = node
     end
-    
-    #if node.next == nil ###
-    #  @tail = node
-    #  node.next = nil
-    #else
-    #  node.next
-    #end
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
-    #empty
-    if @tail == nil
-      return
-    #one item
-    elsif @tail == @head
-      @tail = nil
-      @head = nil
-    #multiple.. need for find the tail
-    else
-      temp = @head
-        while @head && !temp=@head.next
-          temp = temp.next
-          @head = temp
-        end
+    temp = @head.next
+    while !temp=@tail
+      temp = temp.next
+      if temp.next == nil
+        temp = temp
+        @tail = temp
+      end
+      return @head
     end
-    return temp
   end
 
   # This method prints out a representation of the list.
   def print
-    result = ""
-    while !@head=@tai
-      result << @head.to_s
-      @head = @head.next
+    node = @head
+    while node!=nil
+      puts "{node.data}\n"
+      node = node.next
     end
-    return result.to_str
   
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
-    temp2 = @head
-    temp = @head.next
-    if node ==temp.next
-      temp2 = temp
-      temp = temp.next
-      temp2.next = temp.next
-    end
-    #if @head == nil
-    #  return
-    #else
-    #  temp = @head
-    #  while !temp=node
-    #    temp = temp.next
-    #    if temp == node
-    #      temp_t = temp.next
-    #  end
-    #  return 
-      #start = @head
-      #if start.next == node
-      #  @tail = start
-      #else
-      #  start = start.next
-      #end
-    #  return @head
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
@@ -100,7 +61,7 @@ class LinkedList
       @head = node
       node.next = temp
     end
-    return @head
+    return @head   
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
