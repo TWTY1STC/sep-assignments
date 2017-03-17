18fd7695e39862559fc50a1c7f5ceb70510f3010
twty1stc <liza.njuguna@gmail.com>
2017-03-17 22:14:17 +0000

    notate hash class

--- 04-hashes-part-1/hashclass.rb
+++ 04-hashes-part-1/hashclass.rb
@@ -6,12 +6,12 @@ class HashClass
     @items = Array.new(size)
   end
 
-  def []=(key, value)  #<--explain this notation
+  def []=(key, value)  #<--explain this notation ; insert
     #items[] = key, value
   end
 
 
-  def [](key) #< insert?
+  def [](key) #< retrieve. 
   end
 
   def resize
@@ -21,11 +21,19 @@ class HashClass
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    hashcode = 
+    if size < 5
+  
+    
+    end
+    return hashcode
+    #hash function & get index of where hashed value should go based on size of array. 
+    #determine resizing and insert location
   end
 
   # Simple method to return the number of items in the hash
   def size
-    
+    return @items.count
   end
 
 end
