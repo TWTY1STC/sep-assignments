9f83f2c75117ec119eadc2dac0047595a85f274c
twty1stc <liza.njuguna@gmail.com>
2017-03-29 23:10:05 +0000

    implement hash function

--- 04-hashes-part-1/hashclass.rb
+++ 04-hashes-part-1/hashclass.rb
@@ -6,8 +6,10 @@ class HashClass
     @items = Array.new(size)
   end
 
-  def []=(key, value)  #<--explain this notation ; insert
+  def []=(key, value)  #<-- insert
     #items[] = key, value
+    #myhash = HashClass.new()
+    #myhash[key, value] <- th
   end
 
 
@@ -15,20 +17,29 @@ class HashClass
   end
 
   def resize
+    # increase the size of the hash array
+    
+    #recalibrate the hash/index of all elements
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
-    hashcode = 
-    if size < 5
-  
+    #hashcode is the location the key. this needs to be returned
+    #split the key up (loop thru)
     
-    end
-    return hashcode
+    #find the ascii value of each digit/char in the key
+    #combine all the ascii values into a number string...this is the unique hashcode. return it.the hash
     #hash function & get index of where hashed value should go based on size of array. 
     #determine resizing and insert location
+    hashcode = 0
+    
+    key.each_byte do |c|
+      hashcode += c
+    end
+    index = hashcode % size 
+    return index
   end
 
   # Simple method to return the number of items in the hash
